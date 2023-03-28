dd12a8da7c87cd3e580231e5f1e5d61637d5f2a3
DESKTOP-P8G1MPM\Withbrother_GN_02 <jiwon@withbrother.co.kr>
2023-03-28 15:55:26 +0900

    프리티어수정

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,20 +1,20 @@
-import React from 'react';
-import * as Styled from './styled';
+import React from "react";
+import * as Styled from "./styled";
 
-export default function Button({item, onClick}) {
-    return (
-      <>
-        <Styled._Button
+export default function Button({ item, onClick }) {
+  return (
+    <>
+      <Styled._Button
         value={item.text}
         key={item.index}
         type="button"
         background={item.background}
         color={item.color}
         onClick={onClick}
-        className={item.text === '0' ? 'zero' : ''}
-        >
-          <div>{item.text}</div>
-        </Styled._Button>
-      </>
-    );
+        className={item.text === "0" ? "zero" : ""}
+      >
+        <div>{item.text}</div>
+      </Styled._Button>
+    </>
+  );
 }
