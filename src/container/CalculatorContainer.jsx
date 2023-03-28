f64f5fda1d8329ea46931734c8a3f3773d5b473a
DESKTOP-P8G1MPM\Withbrother_GN_02 <jiwon@withbrother.co.kr>
2023-03-28 11:32:11 +0900

    컴포넌트수정

--- src/container/CalculatorContainer.jsx
+++ src/container/CalculatorContainer.jsx
@@ -1,8 +1,37 @@
 import React from 'react';
 import Calculator from '../components/Cacluator/index';
 
+import { useContext } from 'react';
+import { CalculatorContext } from '../context/calculator';
+
 export default function CalculatorContainer() {
+    const { calc, setCalc, shouldSetNumberRef, prevNumberRef } = useContext(CalculatorContext);
+
+    const onClick = (item) => {
+        if (item.type === "number") {
+          setCalc((prev) => {
+            const inputValue = shouldSetNumberRef.current
+              ? item.text
+              : prev.inputValue.replace(/(^0+)/, "") + item.text;
+            shouldSetNumberRef.current = false;
+            return {
+              ...calc,
+              inputValue,
+            };
+          });
+        }
+        if (item.type === "operator") {
+          switch (item.text) {
+            case "+":
+              prevNumberRef.current =
+                Number(prevNumberRef.current) + Number(calc.inputValue);
+              shouldSetNumberRef.current = true;
+              setCalc({ ...calc, inputValue: prevNumberRef.current });
+          }
+        }
+      };
+    
     return (
-        <Calculator />
+        <Calculator calc={calc} onClick={onClick} />
     );
 }
